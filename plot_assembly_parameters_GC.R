a501dad97df97b0f796fef33e48f589c852ca1eb
gemmacollins <gemma.collins@senckenberg.de>
2021-02-12 16:06:48 +1300

    PCA analyses(2)
    Using "params_to_plot" from Miki's plot_assembly_parameters to perform some PCA analyses. Current status: PCA done on all taxa together. To-do:
    
    Check redundancy analysis
    
    Split matrix to perform separate PCAs for each taxonomic group
    Git stats

--- plot_assembly_parameters_GC.R
+++ plot_assembly_parameters_GC.R
@@ -1,5 +1,3 @@
-pacman::p_load(pacman, tidyverse, GGally, taxize) 
-install.packages("tidyverse")
 library(tidyverse)
 library(GGally)
 library(taxize)
@@ -11,7 +9,7 @@ seq_success <- read_tsv(file = "Report_Full.tsv")
 seq_success$batch <- factor(seq_success$batch, 
                             levels = c("b0","b1", "b2", "b3_b5", "b4", 
                                        "b7", "b8", "b9", "b10"))
-#Gemma: why is b11 not included here?
+#Gemma: why is b11 not included here? ####
 
 
 names(seq_success) <- c("libid", "batch", "phylum", "countgroup", "taxon", "taxid", 
@@ -56,12 +54,13 @@ seq_success %>%
 # Acari[8,6] <- c("Oribatida")
 # Acari[13,5] <- c("Sarcoptiformes")
 # Acari[13,6] <- c("Oribatida")
-
-### Gemma: it could not find Tyrophagus in the itis database, but Tyrophagus is also Sarcoptiformes > Oribatida ? So we should add: 
-### Gemma: Acari[2,5] <- c("Sarcoptiformes")
-### Gemma: Acari[2,6] <- c("Oribatida")
-
-
+# 
+# Gemma: Tyrophagus ####
+# Gemma: it could not find Tyrophagus in the itis database, but Tyrophagus is also Sarcoptiformes > Oribatida ? So we should add: 
+# Gemma: Acari[2,5] <- c("Sarcoptiformes")
+# Gemma: Acari[2,6] <- c("Oribatida")
+# 
+# 
 # save(file = "Acari_orders.Rdata", Acari)
 load(file = "Acari_orders.Rdata")
 
@@ -88,169 +87,8 @@ deeper_sequenced %>%
   filter(!(countgroup_2 %in% very_few)) ->
   params_to_plot
 
-
-##############  GEMMA start ##############
-##make a matrix for PCA with all taxonomic groups and all variables
-#want countgroup_2 to be the row names (they are the 9 taxonomic groups) and the 6 variables to be each of the columns
-head(params_to_plot)
-typeof(params_to_plot)
-dim(params_to_plot)
-unique(params_to_plot$countgroup_2)
-
-#generate numerical matrix (pca_mx)
-pca_mx <- matrix(unlist(params_to_plot), ncol=12)
-head(pca_mx)
-typeof(pca_mx)
-rownames(pca_mx) <- params_to_plot$countgroup_2
-colnames(pca_mx) <- colnames(params_to_plot)
-head(pca_mx)
-pca_mx <- pca_mx[,-c(1:6)]
-head(pca_mx)
-class(pca_mx)<-"numeric"
-
-######columns 5 and 6 have NA values and there are several ways to deal with this:
-sum(is.na(pca_mx))
-
-####1 run the pca analysis on only the first 4 columns (with no missing data)
-pc_first4 <- prcomp(pca_mx[,c(1,2,3,4)], center = TRUE, scale = TRUE)
-
-#can graph this using the countgroup_2 categories (from params_to_plot) because the number of rows is the same 
-ggbiplot(pc_first4)
-ggbiplot(pc_first4, obs.scale=1, var.scale = 1, groups = params_to_plot$countgroup_2, ellipse = TRUE, circle = TRUE)+
-  ggtitle("all entries from params_to_plot, first 4 variables")
-
-####2 replace NAs with 0
-pca_mx0 <- pca_mx
-pca_mx0[is.na(pca_mx0)] <- 0
-pc_0 <- prcomp(pca_mx0, center = TRUE, scale = TRUE)
-
-####3 run an imputation to replace NAs with mean values from complete data (but this may be no good - differences between taxnomic groups!)
-# if (!requireNamespace("BiocManager", quietly = TRUE))
-#   install.packages("BiocManager")
-# BiocManager::install("pcaMethods")
-#browseVignettes("pcaMethods")
-library(pcaMethods)
-
-pc <- pca(pca_mx, method="ppca",center = TRUE)
-imputed <- completeObs(pc)
-head(imputed)
-
-# #write table to file so we can see what the NAs were replaced with 
-# library(MASS)
-# write.table(imputed, file = "imputed-matrix.csv", sep = ",", row.names = TRUE)
-
-#run a new pca analysis on the imputed data matrix
-pc_imputed <- prcomp(imputed, center = TRUE, scale = TRUE)
-
-#plot this with groups coloured
-ggbiplot(pc_imputed)
-ggbiplot(pc_imputed, obs.scale=1, var.scale = 1, groups = params_to_plot$countgroup_2, ellipse = TRUE, circle = TRUE)+
-  ggtitle("all")
-
-####4 omit cases with missing data (NAs)
-pc_omit <- prcomp(na.omit(pca_mx), center = TRUE, scale = TRUE)
-
-#So, we have 4 possible pca analyses that we can plot:
-pc_result <- pc_first4
-pc_result <- pc_0
-pc_result <- pc_imputed
-pc_result <- pc_omit
-
-ggbiplot(pc_result, obs.scale=1, var.scale = 1)
-
-summary(pc_result)
-str(pc_result)
-plot(pc_result)
-
-install.packages("devtools")
-library(devtools)
-install_github("vqv/ggbiplot")
-library(ggbiplot)
-
-ggbiplot(pc)
-ggbiplot(pc, obs.scale=1, var.scale = 1, groups = params_to_plot$countgroup_2)+
-  ggtitle("all")
-
-####Overall results####
-# Get summary stats
-summary(pc_result)
-
-# Screeplot for number of components
-plot(pc_result)
-
-# Get standard deviations and rotation
-pc_result
-
-# See how cases load on PCs
-predict(pc_result) %>% round(2)
-
-# Biplot of first two components
-biplot(pc_result)
-
-## plot that shows the PCs and the variation:
-plot(pc_result$x[,1], pc_result$x[,2])
-
-#calculate how much variation in the original data the pcs count for
-pca.var <- pc_result$sdev^2
-pca.var.per <- round(pca.var/sum(pca.var)*100, 1)
-barplot(pca.var.per, main="Scree Plot", xlab="Principal Component", ylab="Percent Variation")
-
-library(ggplot2)
-pca.data <- data.frame(Sample=rownames(pc_result$x),
-                       X=pc_result$x[,1],
-                       Y=pc_result$x[,2])
-pca.data
-
-ggplot(data=pca.data, aes(x=X, y=Y, label=Sample)) +
-  geom_text() +
-  xlab(paste("PC1 - ", pca.var.per[1], "%", sep="")) +
-  ylab(paste("PC2 - ", pca.var.per[2], "%", sep=""))
-  #+ggtitle("")
-
-#to determine which variables have the largest effect on where samples are plotted
-loading_scores <- pc_result$rotation[,1]
-var_scores <- abs(loading_scores) ## get the magnitudes
-var_scores_ranked <- sort(var_scores, decreasing=TRUE)
-top_3_variables <- names(var_scores_ranked[1:3])
-top_3_variables ## show the names of the top 3 variables
-
-####Stop here for now ####
-##generate data frame (so taxa name can be grouping variable)
-pca_df <- data.frame(matrix(unlist(params_to_plot), ncol=12))
-head(pca_df)
-colnames(pca_df) <- colnames(params_to_plot)
-head(pca_df)
-
-pca_df <- pca_df[,-c(1:3, 5,6)]
-
-class(pca_df) <- "numeric"
-pc <- prcomp(pca_df[,c(1:4)], center = TRUE, scale = TRUE)
-
-
-#rubbish/workings
-##group the rows by the taxonomic group (x9)
-by_taxgroup <- params_to_plot %>% group_by(countgroup_2)
-by_taxgroup
-ungroup(params_to_plot)
-
-##now, try to make the matrix again but only with one group at a time
-
-#Collembola
-pca_collembola <- matrix(unlist(params_to_plot), ncol=12)
-
-# install.packages("dplyr")
-# library(dplyr)
-df %>% group_by()
-
-install.packages("plyr")
-library(plyr)
-
-split(params_to_plot, params_to_plot$countgroup_2)
-params_to_plot$Tardigrada
-
-
-
-##############  GEMMA end ##############
+# Gemma: Save params_to_plot for PCA to be a separate script ####
+# save(file = "params_to_plot.Rdata", params_to_plot)
 
 
 # Histograms
